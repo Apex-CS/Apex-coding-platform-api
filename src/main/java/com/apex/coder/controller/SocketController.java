8eb5b69a3f320991237f70edcb54e0e282b40e68
Angel Flores <anflores@apexsystems.com>
2024-02-27 12:13:16 -0600

    Initial commit

---
+++ src/main/java/com/apex/coder/controller/SocketController.java
@@ -0,0 +1,33 @@
+package com.apex.coder.controller;
+
+import com.apex.coder.dto.sockets.OutputMessage;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.handler.annotation.Header;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.messaging.simp.annotation.SendToUser;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@CrossOrigin
+@Slf4j
+@RestController
+public class SocketController {
+    @Autowired
+    SimpMessagingTemplate messagingTemplate;
+
+    @MessageMapping("/broadcast")
+    @SendTo("/topic/reply")
+    public String broadcastMessage(@Payload final String message, @Header("simpSessionId") String sessionId) throws Exception {
+        log.info(message);
+        log.info(sessionId);
+        return "You have received a message: " + message;
+        /*OutputMessage out = new OutputMessage(sessionId, message);
+        messagingTemplate.convertAndSend(
+                message);*/
+    }
+}
